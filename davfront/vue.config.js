056deba40cbea5526f4a2ad5f4a5dcf67b5dd6c4
Mapacher <Davidrcabello92@gmail.com>
2024-12-17 15:32:48 +0100

    cambios deploy

--- davfront/vue.config.js
+++ davfront/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/Web-Gestion-mock/davfront'
+      ? '/Da-rc.github.io/davfront'
       : '/',
 })
